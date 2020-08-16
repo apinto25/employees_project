8d5e0af600c84b7c60112667ba2592af628650b7
Ana Pinto <ana.pinto@alternova.io>
2020-08-16 18:42:06 -0500

    Created person and department models, configure with postgresql db.

--- apps/department/models.py
+++ apps/department/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField('Name', max_length=50)
+    short_name = models.CharField('Short name', max_length=20)
+
+    def __str__(self):
+        return str(self.id) + self.name
--- apps/person/models.py
+++ apps/person/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from apps.department.models import Department
+
+
+JOB_CHOICES = (
+    ('0', 'Accountant'),
+    ('1', 'Administrator'),
+    ('2', 'Economist'),
+    ('3', 'Other')
+)
+
+class Employee(models.Model):
+    first_name = models.CharField('First name', max_length=60)
+    last_name = models.CharField('Last name', max_length=60)
+    job = models.CharField('Job', max_length=50, choices=JOB_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    #image = models.ImageField(upload_to=None, height_field=None, width_field=None)
+
+    def __str__(self):
+        return str(self.id) + self.first_name + self.last_name
