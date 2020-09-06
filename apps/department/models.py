443159cc0940b61cb5a5ecd4a4cd9fd0a141abc1
Ana Pinto <anapinto2503@gmail.com>
2020-09-06 11:51:10 -0500

    Added dept model

--- apps/department/models.py
+++ apps/department/models.py
@@ -5,5 +5,11 @@ class Department(models.Model):
     name = models.CharField('Name', max_length=50)
     short_name = models.CharField('Short name', max_length=20)
 
+    class Meta:
+        verbose_name = 'Department'
+        verbose_name_plural = 'Departments of the company'
+        ordering = ['name']
+        unique_together = ('name', 'short_name')
+
     def __str__(self):
-        return str(self.id) + self.name
+        return str(self.id) + '-' + self.name
