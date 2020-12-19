3d05976d98c554017fc06bfba66b3670318945ef
Ana Pinto <anapinto2503@gmail.com>
2020-12-19 12:47:07 -0500

    Added pagination to ListAllEmployees view.

--- apps/person/views.py
+++ apps/person/views.py
@@ -6,17 +6,19 @@ from apps.person.models import Employee
 
 class ListAllEmployees(ListView):
     """List all employees of the company."""
-    template_name = 'person/list_all.html'
+    template_name = "person/list_all.html"
+    paginate_by = 10
+    ordering = "first_name"
     model = Employee
 
 
 class ListEmployeesByArea(ListView):
     """List all Employees from an area of the company."""
-    template_name = 'person/list_by_area.html'
+    template_name = "person/list_by_area.html"
 
     def get_queryset(self):
 
-        area = self.kwargs['short_name']
+        area = self.kwargs["short_name"]
         queryset = Employee.objects.filter(
             department__name=area
         )
@@ -25,8 +27,8 @@ class ListEmployeesByArea(ListView):
 
 class ListEmployeesByKword(ListView):
     """List employees by keyword from html template."""
-    template_name = 'person/list_by_kword.html'
-    context_object_name = 'employees'
+    template_name = "person/list_by_kword.html"
+    context_object_name = "employees"
 
     def get_queryset(self):
         keyword = self.request.GET.get("kword",)
